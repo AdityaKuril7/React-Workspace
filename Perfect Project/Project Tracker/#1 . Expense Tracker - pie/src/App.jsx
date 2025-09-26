9f2edc88da0c83983c4035f61ee1b0fbabc48810
AdityaKuril7 <adityakuril760@gmail.com>
2025-09-26 23:43:13 +0530

    add Exepnse Tracker working

--- Perfect Project/Project Tracker/#1 . Expense Tracker - pie/src/App.jsx
+++ Perfect Project/Project Tracker/#1 . Expense Tracker - pie/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 function Buttons(props) {
